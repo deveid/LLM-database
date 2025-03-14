55378cd38daf53e968e13c54efac3e897dbfe3ac
deveid <deveidtolu18@gmail.com>
2025-03-14 13:16:11 -0300

    add base

---
+++ app.py
@@ -0,0 +1,43 @@
+import streamlit as st
+from llm import execute_user_query
+# Sample images for dropdown
+image_options = {
+    "dvdrental": "/Users/david/Documents/langchain/test/images/dvdrental.png",
+}
+
+response_options = {
+        "dvdrental": "Sure! Here’s some information from the dvdrental database.",
+        "medicore": "Fetching medical data for you...",
+        "finledger": "Accessing financial records...",
+    }
+
+# dropdown choices for text response
+def query_database(database, question):
+    return response_options.get(database, "Sorry, no data available.")
+
+# Divide the page into two equal columns
+col1, col2 = st.columns(2)
+
+# Left Side: Image Selection
+with col1:
+    st.subheader("Database Schema")
+    selected_image = st.selectbox("Choose the database to see schema:", list(image_options.keys()))
+    st.image(image_options[selected_image], caption=selected_image, use_container_width=True)
+
+# Right Side: Text Input & Dropdown
+with col2:
+
+    st.subheader("User Input & Response")
+    db_selected_response = st.selectbox("Choose a database:", list(response_options.keys()))
+
+    user_input = st.text_input("Enter question you'd like to ask from the database:")
+
+    if st.button("Submit"):
+        if not user_input.strip():
+            st.error("Please enter a question before submitting.")
+        else:
+            with st.spinner("Processing your request..."):
+                response = execute_user_query(db_name=db_selected_response, question=str(user_input))
+            st.write("### Response Query:", response[0])
+            st.write("### Response:", response[1])
+            st.success(response[1])
---
+++ llm.py
@@ -0,0 +1,39 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+from langchain.chains import create_sql_query_chain
+from langchain_community.llms import Ollama
+from langchain_community.tools.sql_database.tool import QuerySQLDatabaseTool
+from langchain_experimental.sql import SQLDatabaseChain
+from langchain_community.utilities.sql_database import SQLDatabase
+from sqlalchemy import create_engine
+from langchain.sql_database import SQLDatabase
+load_dotenv()
+
+OLLAMA_SERVER_URL="http://localhost:11434"
+llm_model=Ollama(model="gemma3:4b", temperature=0.1, base_url=OLLAMA_SERVER_URL)
+
+
+def extract_sql(text):
+    words_to_remove = ["SQLQuery:", "sql"]
+    for word_to_remove in words_to_remove:
+        text = text.replace(word_to_remove, "")
+    text = text[text.find("SELECT"): text.find(";")]
+    return text
+
+def execute_user_query(llm=llm_model,db_name= any, question=any):
+
+    POSTGRES_USER="david"
+    POSTGRES_PASSWORD=""
+    POSTGRES_HOST="127.0.0.1"
+    POSTGRES_PORT="5431"
+    POSTGRES_DB=db_name
+    connection_string = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+    engine = create_engine(connection_string)
+    db = SQLDatabase(engine)
+
+    generate_query = create_sql_query_chain(llm,db)
+    query=generate_query.invoke({"question": question})
+    execute_query = QuerySQLDatabaseTool(db=db)
+    cleaned_query = extract_sql(query)
+    result = execute_query.invoke(cleaned_query)
+    return cleaned_query,result
